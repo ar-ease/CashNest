1b4f8433d8e608135a4dfb93ce1d3b9560360de3
ar-ease <arghya0017@gmail.com>
2025-02-25 21:42:53 +0530

    intial setups

---
+++ my-app/src/app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { Header } from "@/components/header";
+
+const inter = Inter({ subsets: ["latin"] });
+export const metadata: Metadata = {
+  title: "welth",
+  description: "one stop finance platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} pt-32`}>
+        <Header />
+        <main className="min-h-screen">{children}</main>
+
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto text-center">
+            <p>Made with Love </p>
+          </div>
+        </footer>
+      </body>
+    </html>
+  );
+}
---
+++ my-app/src/components/Header.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Header = () => {
+  return <div className="text-blue-650 bg-slate-500">headdsfsfdsfer</div>;
+};
