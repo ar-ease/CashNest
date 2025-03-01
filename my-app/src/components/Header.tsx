88ec6396b82fc842fd655cc2d1318c6b2b5b9b9a
ar-ease <arghya0017@gmail.com>
2025-03-01 19:51:16 +0530

    added dashboard and transaction in header

--- my-app/src/components/Header.tsx
+++ my-app/src/components/Header.tsx
@@ -9,21 +9,32 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
-import { LayoutDashboard } from "lucide-react";
+import { LayoutDashboard, PenBox } from "lucide-react";
 
 export const Header = () => {
+  console.log("hello there");
   return (
     <>
       <div className="fixed top-0 w-full bg-white/80 backdrop-blur-md z-50 border-b">
         <nav className="container mx-auto px-4 py-4 flex items-center justify-between">
           <Link href="/">
-            <Image src={"/logo.png"} alt="logo" width={100} height={100} />
+            <Image src={"/logo.png"} alt="logo" width={100} height={60} />
           </Link>
-          <div>
+          <div className="flex items-center space-x-4">
             <SignedIn>
-              <Link href="/dashboard">
+              <Link
+                href={"/dashboard"}
+                className=" text-gray-600 hover:text-blue-600 flex items-center gap-2"
+              >
                 <Button variant={"outline"}>
-                  <LayoutDashboard />
+                  <LayoutDashboard size={18} />
+                  dashboard
+                </Button>
+              </Link>
+              <Link href={"/transactions/create"} className="">
+                <Button>
+                  <PenBox size={18} />
+                  transactions
                 </Button>
               </Link>
             </SignedIn>
@@ -34,7 +45,13 @@ export const Header = () => {
               {/* <SignUpButton /> */}
             </SignedOut>
             <SignedIn>
-              <UserButton />
+              <UserButton
+                appearance={{
+                  elements: {
+                    avatarBox: "w-10 h-10",
+                  },
+                }}
+              />
             </SignedIn>
           </div>
         </nav>
