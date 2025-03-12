76ea956ccd67402d785df1a20d5ccd9fe4e27036
ar-ease <arghya0017@gmail.com>
2025-03-12 12:46:33 +0530

    added main layout and nf

---
+++ my-app/src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const NotFound = () => {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-[80vh]">
+      <div className=" ">
+        <h1 className="text-2xl font-semibold">
+          OOPS...This page is not Found{" "}
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
